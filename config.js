97b723ef4aef125ac43b3a333106480d078cc9f2
uraniumreza <reza.uranium@gmail.com>
2019-08-12 13:50:29 +0600

    Add imgur image

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   // the description of the website (eg. what shows on Google)
   description: "Nayeem Reza's blog",
   // this will be used as og:image
-  image: 'https://www.dropbox.com/s/vjgmf3yveijwhho/social-share.png',
+  image: 'https://i.imgur.com/VgAAYIK.png',
   // a short bio shown at the bottom of your blog posts
   // It should complete the sentence: Written by Nayeem Reza ...
   shortBio: '',
